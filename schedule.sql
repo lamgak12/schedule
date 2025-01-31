01974a3b914bbceadbef09cfb8dbe0047432f062
lamgak12 <tntnqk12@naver.com>
2025-01-31 21:18:05 +0900

    일정 테이블 쿼리

---
+++ schedule.sql
@@ -0,0 +1,8 @@
+CREATE TABLE schedules (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '일정 관리 식별자',
+    name VARCHAR(100) NOT NULL COMMENT '작성자 이름',
+    password VARCHAR(100) NOT NULL COMMENT '게시글 비밀번호 (수정/삭제 시 필요)',
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '게시글 생성일',
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '게시글 수정일',
+    contents TEXT NOT NULL COMMENT '할 일 내용'
+) COMMENT='일정 관리 테이블';
