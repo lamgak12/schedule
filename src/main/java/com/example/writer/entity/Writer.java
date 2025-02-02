e7cf4e1e30feaff2b45b2d5cd3f1da7658c01f8b
lamgak12 <tntnqk12@naver.com>
2025-02-03 05:15:45 +0900

    feat: Writer 엔티티 추가

---
+++ src/main/java/com/example/writer/entity/Writer.java
@@ -0,0 +1,23 @@
+package com.example.writer.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+
+@Getter
+@AllArgsConstructor
+public class Writer {
+    private Long id;
+    private String name;
+    private String email;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    public Writer(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.createdAt = new Timestamp(System.currentTimeMillis());  // createdAt 초기화
+        this.updatedAt = new Timestamp(System.currentTimeMillis());  // updatedAt 초기화
+    }
+}
